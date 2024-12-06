4cb575c467a4bb74d62b37a475ebbebd36adf851
Luce <boama561@gmail.com>
2024-12-06 01:03:16 +0000

    Reglages des responsives mis a jour des media query, style du bouton menu, ouverture vers la droite. correction du navBar, logo, et bouton menuBurger

--- js/menuBurger.js
+++ js/menuBurger.js
@@ -5,5 +5,14 @@ const burgerMenu = document.querySelector('.burger-menu')
 burgerMenuButton.onclick = function () {
     burgerMenu.classList.toggle('open')
     const isOpen = burgerMenu.classList.contains('open')
-    burgerMenuButtonIcon.classList = isOpen ?  'ri-close-large-line' : 'ri-menu-fill'
+    burgerMenuButtonIcon.className = isOpen ?  'ri-close-large-line' : 'ri-menu-fill'
 }
+
+// Fermer le menu en cliquant à l'extérieur
+document.addEventListener('click', function (event) {
+    if (!burgerMenuButton.contains(event.target) && !burgerMenu.contains(event.target)) {
+        burgerMenu.classList.remove('open');
+        document.body.classList.remove('menu-open'); // Retire la classe "menu-open"
+        burgerMenuButtonIcon.className = 'ri-menu-fill'; // Réinitialise l'icône
+    }
+});
