c1ed1d67dd9cc92670c744c266eb7ba55f7038f8
Luce <boama561@gmail.com>
2025-03-20 15:31:49 +0000

    Ajout d'une animation js pour les elements card de Services

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,5 @@
 // Appliquer l'animation correcte au chargement de la page
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     const allElements = document.querySelectorAll('[data-aos]');
     const isMobile = window.innerWidth <= 910; // Détecter les petits écrans dès le chargement
 
@@ -21,7 +21,7 @@ window.addEventListener('load', function() {
 });
 
 // Appliquer l'animation correcte lors du redimensionnement de la fenêtre
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
     const allElements = document.querySelectorAll('[data-aos]');
     const isMobile = window.innerWidth <= 910;
 
@@ -55,4 +55,35 @@ document.addEventListener("DOMContentLoaded", function () {
     );
 
     observer.observe(socialSection);
+});
+
+// Declencher une animation pour les elements Card.
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".card");
+
+        let threshold = 0.5; // Définir un seuil par défaut pour les grands écrans (50%)
+
+    // Vérifier la taille de l'écran et ajuster le seuil
+    if (window.innerWidth <= 700) {
+        threshold = 0.3; // Réduire le seuil à 30% pour les petits écrans
+    }
+
+    // Créer l'observer avec le seuil dynamique
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const card = entry.target;
+                    const index = Array.from(cards).indexOf(card); // Trouve l'index de la carte
+                    setTimeout(() => {
+                        card.classList.add("visible");
+                    }, index * 300); // Décalage progressif entre chaque carte
+                    observer.unobserve(card); // Arrêter l'observation de la carte une fois animée
+                }
+            });
+        },
+        { threshold: threshold } // Utilise le seuil dynamique ici
+    );
+
+    cards.forEach(card => observer.observe(card)); // Observer chaque carte individuellement
 });
